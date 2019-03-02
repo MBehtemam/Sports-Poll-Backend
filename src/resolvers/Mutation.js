26bf07e9a0d3708162a109c4f70ccce4b32b36e1
MBehtemam <ehtemam@gmail.com>
2019-03-02 11:41:32 +0330

    adding createGroup mutaion

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,11 +1,17 @@
 const Mutation = {
   async createCountry(parent, args, ctx, info) {
-    console.log(args);
     const country = await ctx.db.mutation.createCountry(
       { data: { ...args } },
       info
     );
     return country;
+  },
+  async createGroup(parent, args, ctx, info) {
+    const group = await ctx.db.mutation.createGroup(
+      { data: { ...args } },
+      info
+    );
+    return group;
   }
 };
 module.exports = Mutation;
