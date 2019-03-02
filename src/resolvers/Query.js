38776c25584fb21db1e432c0b83dadee24544b20
MBehtemam <ehtemam@gmail.com>
2019-03-02 11:59:00 +0330

    adding groups and coutries queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const Query = {
+  async countries(parent, args, ctx, info) {
+    const r = await ctx.db.query.countries();
+    return r;
+  },
+  async groups(parent, args, ctx, info) {
+    const r = await ctx.db.query.groups();
+    return r;
+  }
+};
 
 module.exports = Query;
